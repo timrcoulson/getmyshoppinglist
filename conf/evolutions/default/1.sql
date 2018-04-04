bc516ef299631110d5b8e0fbf0da26ceaa249952
Tim Coulson <tim.r.coulson@gmail.com>
2018-04-04 22:07:26 +0100

    chore: migrations to postgres

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,62 +1,59 @@
-# PRODUCTS schema
+-- PRODUCTS schema
 
-# --- !Ups
+-- --- !Ups
 
-CREATE TABLE products (
-  id            BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  ingredient_id BIGINT(20)   NOT NULL,
+CREATE TABLE "products" (
+  id            BIGSERIAL primary key,
+  ingredient_id BIGSERIAL   NOT NULL,
   name          VARCHAR(255) NOT NULL,
-  quantity      BIGINT(20)   NOT NULL,
+  quantity      BIGINT   NOT NULL,
   unit          VARCHAR(255) NOT NULL,
-  price         BIGINT(20)   NOT NULL,
-  url           VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  price         BIGINT   NOT NULL,
+  url           VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE product_sources (
+CREATE TABLE "product_sources" (
   name VARCHAR(255) NOT NULL,
   type VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE recipes (
-  id                BIGINT(20)   NOT NULL AUTO_INCREMENT,
+CREATE TABLE "recipes" (
+  id            BIGSERIAL primary key,
   name              VARCHAR(255) NOT NULL,
   link              VARCHAR(255) NOT NULL,
-  serves            BIGINT(20)   NOT NULL,
+  serves            BIGINT   NOT NULL,
   active            BOOL         NOT NULL,
-  alt_cost_per_head BIGINT(20)   NOT NULL,
+  alt_cost_per_head BIGINT   NOT NULL,
   img               VARCHAR(255) NOT NULL,
   description       VARCHAR(255) NOT NULL,
   method            VARCHAR(255) NOT NULL,
   prep_time         VARCHAR(255) NOT NULL,
   cooking_time      VARCHAR(255) NOT NULL,
   type              VARCHAR(255) NOT NULL,
-  deleted_at        DATETIME     NOT NULL,
-  PRIMARY KEY (id)
+  deleted_at        TIMESTAMP(0)     NOT NULL
 );
 
-CREATE TABLE recipes_ingredients (
-  ingredient_id BIGINT(20)   NOT NULL,
-  recipe_id     BIGINT(20)   NOT NULL,
+CREATE TABLE "recipes_ingredients" (
+  ingredient_id BIGSERIAL   NOT NULL,
+  recipe_id     BIGSERIAL   NOT NULL,
   unit          VARCHAR(255) NOT NULL,
   scales        VARCHAR(255) NOT NULL,
-  quantity      BIGINT(20)   NOT NULL
+  quantity      BIGINT   NOT NULL
 );
 
-CREATE TABLE ingredients (
-  id    BIGINT(20)   NOT NULL AUTO_INCREMENT,
+CREATE TABLE "ingredients" (
+  id    BIGSERIAL primary key,
   name  VARCHAR(255) NOT NULL,
   aisle VARCHAR(255) NOT NULL,
   keeps BOOL         NOT NULL,
   exact BOOL         NOT NULL,
-  url   VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  url   VARCHAR(255) NOT NULL
 );
 
-# --- !Downs
+-- --- !Downs
 
-DROP TABLE products;
-DROP TABLE product_sources;
-DROP TABLE recipes;
-DROP TABLE ingredients;
+DROP TABLE "products";
+DROP TABLE "product_sources";
+DROP TABLE "recipes";
+DROP TABLE "ingredients";
